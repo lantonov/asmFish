b60e1ad018db507b03da9ac31d95c05182d87fe8
Lyudmil Antonov <lantonov.here@gmail.com>
2016-12-17 10:08:42 +0000

    Add new executables

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-"asmFish\fasm.exe" "asmFish\asmFishW_base.asm"      -m 50000 "Windows\asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_base.exe"
+"asmFish/fasm.exe" "asmFish/asmFishW_base.asm"      -m 50000 "Windows/asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_base.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_popcnt.asm"    -m 50000 "Windows\asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_popcnt.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_bmi2.asm"      -m 50000 "Windows\asmFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_bmi2.exe"
 "asmFish\fasm.exe" "asmFish\pedantFishW_base.asm"   -m 50000 "Windows\pedantFishW_%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%_base.exe"
