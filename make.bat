d01a0151ca59756393c7c91ead980c67f67769a4
Lyudmil Antonov <lantonov.here@gmail.com>
2017-01-03 15:31:23 +0200

    Update make.bat

--- make.bat
+++ make.bat
@@ -2,7 +2,7 @@
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
-set "datestamp=%YYYY%-%MM%-%DD%" & set "timestamp=%HH%:%Min%:%Sec%" & set "fullstamp=%YYYY%-%MM%-%DD%_%HH%:%Min%:%Sec%"
+set "datestamp=%YYYY%-%MM%-%DD%" & set "timestamp=%HH%%Min%%Sec%" & set "fullstamp=%YYYY%-%MM%-%DD%_%HH%%Min%%Sec%"
 
 "asmFish\fasm.exe" "asmFish\asmFishW_base.asm"      -m 50000 "Windows\asmFishW_%datestamp%_base.exe"
 "asmFish\fasm.exe" "asmFish\asmFishW_popcnt.asm"    -m 50000 "Windows\asmFishW_%datestamp%_popcnt.exe"
