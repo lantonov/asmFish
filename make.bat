cded39a915f0b1ae32ce0824100b776961f2a393
lantonov <lantonov.here@gmail.com>
2017-10-03 22:39:29 +0100

    Do not clear screen after assembling

--- make.bat
+++ make.bat
@@ -3,9 +3,9 @@ for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "d
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 set "datestamp=%YYYY%-%MM%-%DD%" & set "timestamp=%HH%%Min%%Sec%" & set "fullstamp=%YYYY%-%MM%-%DD%_%HH%%Min%%Sec%"
+cls
 
 :menu
-cls
 echo    EXECUTABLES
 echo    ==========
 echo.
