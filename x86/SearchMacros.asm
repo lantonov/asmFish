7d8f74932c2447cd818d143117ae144086cec956
Counterply <counterply@gmail.com>
2018-07-06 22:01:55 -0400

    Tweak null move pruning conditions bench 5088156
    https://github.com/official-stockfish/Stockfish/commit/de0889612f93d021e0754a201ce751fe05d8cb2c

--- x86/SearchMacros.asm
+++ x86/SearchMacros.asm
@@ -391,6 +391,9 @@ Display	2, "Search(alpha=%i1, beta=%i2, depth=%i8) called%n"
 
 	    ; Step 8. Null move	search with verification search	(is omitted in PV nodes)
   if PvNode = 0
+        mov  edx, dword[rbx-1*sizeof.State+State.statScore]
+        cmp  edx, 30000
+        jge  .8skip
         mov   edx, dword[rbx-1*sizeof.State+State.currentMove]
         cmp   edx, MOVE_NULL
         je  .8skip
