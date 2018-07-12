a3c6d36acb8e5a00229c93af041801a11269460d
Double Beep <38133098+double-beep@users.noreply.github.com>
2018-07-12 21:31:39 +0300

    Realign bench of asmFish_base
    asmFish_base executables should now give the correct bench value of 4777674.
    
    No functional change for other executables.

--- x86/Pawn.asm
+++ x86/Pawn.asm
@@ -172,7 +172,13 @@ Neighbours_False:
 
 Continue:
         _popcnt   rax, r8, r9
-        _popcnt   r9, rbx, r10
+    if CPU_HAS_POPCNT = 1
+         popcnt   r9, rbx
+    else
+           push   r10             
+         _popcnt   r9, rbx, r10
+            pop   r10
+    end if
 
             neg   r11d
             neg   rbx
